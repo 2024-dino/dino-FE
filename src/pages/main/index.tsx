baa9079e0808fe2dcded0b1980945cff4c36df9c
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-09-23 02:55:06 +0900

    feat: #11 - 촬영 초기 미리보기 ui 숨김, 촬영 사진 사용 여부 로직 구현

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -38,7 +38,7 @@ export default function MainPage<Props>({}) {
           setIsOpen={setIsQuestionModalOpen}
         />
       )}
-      {isCameraOn && <CameraModalPro />}
+      {isCameraOn && <CameraModalPro onClose={() => setIsCameraOn(false)}/>}
     </>
   );
 }
