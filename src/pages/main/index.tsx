b29be556a69543a3520808052d477120b29afb1b
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-09-24 18:30:23 +0900

    Refactor: 헤더 컴포넌트 flex 변경

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,30 +1,30 @@
-import NavBar from '@/components/common/NavBar';
+// main/index
+
+import CameraModalPro from '@/components/main/CameraModalPro';
 import DateChanger from '@/components/DateChanger';
 import EventElement from '@/components/eventManage/EventElement';
+import EventList from '@/components/main/EventList';
+import Header from '../../components/main/Header';
+import NavBar from '@/components/common/NavBar';
+import QuestionModal from '@/components/main/QuestionModal';
 import SlideMenu from '@/components/main/SideMenu';
 import TimePicker from '@/components/TimePicker';
-import { useState } from 'react';
-import Header from '../../components/main/Header';
-import EventList from '@/components/main/EventList';
-import QuestionModal from '@/components/main/QuestionModal';
 import Waveform from '@/components/main/Waveform';
-import CameraModalPro from '@/components/main/CameraModalPro';
+import { useState } from 'react';
 
 interface Props {}
 
 export default function MainPage<Props>({}) {
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
   const [isQuestionModalOpen, setIsQuestionModalOpen] = useState(false);
-  const [isCameraOn, setIsCameraOn] = useState(true);
+  const [isCameraOn, setIsCameraOn] = useState(false);
 
   const handleTimeChange = () => {
     return;
   };
   return (
     <>
-      <div
-        className={`flex flex-col text-center w-full h-screen items-center justify-center relative`}
-      >
+      <div className={`flex flex-col text-center w-full h-screen items-center`}>
         <Header onClick={() => setIsSideMenuOpen(true)} />
         <SlideMenu isOpen={isSideMenuOpen} setIsOpen={setIsSideMenuOpen} />
         <EventElement />
@@ -36,9 +36,10 @@ export default function MainPage<Props>({}) {
         <QuestionModal
           isOpen={isQuestionModalOpen}
           setIsOpen={setIsQuestionModalOpen}
+          setIsCameraOn={()=>setIsCameraOn(true)}
         />
       )}
-      {isCameraOn && <CameraModalPro onClose={() => setIsCameraOn(false)}/>}
+      {isCameraOn && <CameraModalPro onClose={() => setIsCameraOn(false)} />}
     </>
   );
 }
