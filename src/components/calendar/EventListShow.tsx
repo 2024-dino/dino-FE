d677a4151df1e2ecc275c8ae39cfef0c24f607b2
ssongms <songms0909@khu.ac.kr>
2024-09-25 10:47:42 +0900

    Refactor #3 - 캘린더 CSS 일부 조정

--- src/components/calendar/EventListShow.tsx
+++ src/components/calendar/EventListShow.tsx
@@ -13,18 +13,18 @@ const EventListShow = ({ events }: Props) => {
           className="w-full flex flex-col items-center justify-center"
         >
           {/* title */}
-          <div className="w-[calc(100%-60px)] h-[20px] mb-[8px]">
+          <div className="w-[calc(100%-60px)] h-[20px] mb-3">
             <span className="font-pretendard-400 text-[#888] text-[14px]">
               {event.title}
             </span>
           </div>
 
           <div
-            className="w-[calc(100%-40px)] h-[80px] mb-[20px] rounded-[10px] bg-white"
+            className="w-[calc(100%-40px)] h-[80px] mb-[25px] rounded-[10px] bg-white"
             style={{ boxShadow: '0px 2px 16px 0px rgba(68, 68, 68, 0.12)' }}
           >
             <div className="w-full h-[80px] flex flex-col items-center justify-center px-[10px]">
-              <div className="w-full items-center flex h-[33px]">
+              <div className="w-full items-center flex h-[26px]">
                 <span className="font-pretendard-300 text-[#BAD7EC] text-[24px]">
                   Q.
                 </span>
@@ -32,7 +32,7 @@ const EventListShow = ({ events }: Props) => {
                   {event.content}
                 </span>
               </div>
-              <div className="w-full flex items-center h-[33px]">
+              <div className="w-full flex items-center h-[26px]">
                 <span className="font-pretendard-300 text-[#BAD7EC] text-[24px]">
                   A.
                 </span>
