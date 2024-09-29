b1bb5c0a8725aadefd3678bb14dcbf7925dd5672
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-09-29 16:22:04 +0900

    fix: 초기 로딩 url 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
 import { Inter } from 'next/font/google';
-import MainPage from './day';
-import { useRouter } from 'next/router';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  const router = useRouter();
-  router.push('/day')
   return <></>;
 }
