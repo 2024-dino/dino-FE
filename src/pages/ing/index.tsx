c4b18ec5726519af3965c970a9abf29d4458cb0e
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2024-10-04 04:12:44 +0900

    feat: #24 - 생성된 이벤트 없을 때 ing 페이지 ui 구현

--- src/pages/ing/index.tsx
+++ src/pages/ing/index.tsx
@@ -6,28 +6,31 @@ import NavBar from '@/components/common/NavBar';
 import { useRouter } from 'next/router';
 import { useGetEvents } from '@/hooks/api/useEvent';
 import { EventType } from '@/types/event';
+import NoEventCard from '@/components/ing/noEventCard';
 
 const IngPage = () => {
   const router = useRouter();
   const { data, isSuccess, error } = useGetEvents();
   return (
-    <div className="w-full h-screen items-center`">
+    <div className="w-full h-screen items-center bg-custom-gradient">
       <Header />
+      {data?.data.length == 0 && <NoEventCard />}
       <div className="flex flex-col w-full px-5 gap-3 items-center">
         {data?.data.map((element: EventType) => (
           <EventCard {...element} />
         ))}
       </div>
 
-      <div className="absolute bottom-[100px] w-full px-5 flex items-center jusify-center">
-        <button
-          onClick={() => router.push('/ing/create')}
-          className="w-full relative shadow-[0_2px_20px_rgba(136,136,136,0.12)] rounded-lg bg-white/80 h-[52px] text-black/60 text-center font-pretendard text-base font-light leading-[120%] tracking-[-0.64px]"
-        >
-          새로운 이벤트 생성
-        </button>
-      </div>
-
+      {data?.data.length < 3 && (
+        <div className="absolute bottom-[100px] w-full px-5 flex items-center jusify-center">
+          <button
+            onClick={() => router.push('/ing/create')}
+            className="w-full relative shadow-[0_2px_20px_rgba(136,136,136,0.12)] rounded-lg bg-white/80 h-[52px] text-black/60 text-center font-pretendard text-base font-light leading-[120%] tracking-[-0.64px]"
+          >
+            새로운 이벤트 생성
+          </button>
+        </div>
+      )}
       <NavBar />
     </div>
   );
